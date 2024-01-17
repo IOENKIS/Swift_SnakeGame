62f70169562e2768f579dd6373c85a0422cf402e
IOENKIS <90310655+IOENKIS@users.noreply.github.com>
2024-01-17 17:17:38 +0200

    Initial Commit

---
+++ SnakeGame/SnakeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnakeGameApp.swift
+//  SnakeGame
+//
+//  Created by IVANKIS on 17.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct SnakeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
